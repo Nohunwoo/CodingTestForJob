eaf008d2bdbe67c9ea68c0b84ea38f7b88eb77d6
Nohunwoo <163727448+Nohunwoo@users.noreply.github.com>
2026-01-30 10:34:20 +0900

    꼬리 문자열.py

--- Python3/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ Python3/프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -4,3 +4,8 @@ def solution(str_list, ex):
         if ex not in i:
             answer += i
     return answer
+
+## 리스트 컴프리헨션으로도 풀 수 있다.
+## def solution(str_list, ex):
+## return "".join(i for i in str_list if ex not in i)
+## 이게 더 빠르고 정확하다
