19381a813792a3d97170a99ff1ac2f57cd534512
Nohunwoo <163727448+Nohunwoo@users.noreply.github.com>
2026-03-23 17:43:34 +0900

    [Bronze I] Title: 단어 공부, Time: 68 ms, Memory: 34368 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,12 @@
+# Bronze 1
+word = input().upper()
+word_set = list(set(word))
+cnt_list = []
+for i in word_set:
+    cnt_list.append(word.count(i))
+
+if cnt_list.count(max(cnt_list)) > 1:
+    print("?")
+else:
+    max_index = cnt_list.index(max(cnt_list))
+    print(word_set[max_index])
