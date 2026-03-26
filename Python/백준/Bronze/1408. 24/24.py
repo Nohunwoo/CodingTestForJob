d36433bd0dff80fa88c9a4ca6b22a279f2a9d28e
Nohunwoo <163727448+Nohunwoo@users.noreply.github.com>
2026-03-26 15:25:02 +0900

    [Bronze II] Title: 24, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1408. 24/24.py
+++ Python/백준/Bronze/1408. 24/24.py
@@ -1,22 +1,14 @@
-import sys
-input = sys.stdin.readline
+start = input()
+end = input()
 
-real_time = input().strip()
-start_time = input().strip()
+starttime = int(start[0:2])*3600 + int(start[3:5])*60+int(start[6:8])
+endtime = int(end[0:2])*3600 + int(end[3:5])*60+int(end[6:8])
 
-r_h, r_m, r_s = map(int, real_time.split(':'))
-s_h, s_m, s_s = map(int, start_time.split(':'))
+realtime = endtime - starttime
+if realtime < 0:
+    realtime += 86400 
+real_hour = realtime // 3600
+real_minute = (realtime - real_hour*3600) // 60
+real_second = (realtime - real_hour*3600 - real_minute*60)
 
-real_sec = r_h * 3600 + r_m * 60 + r_s
-start_sec = s_h * 3600 + s_m * 60 + s_s
-
-left_sec = start_sec - real_sec
-
-if left_sec < 0:
-    left_sec += 24 * 3600 
-
-ans_h = left_sec // 3600
-ans_m = (left_sec % 3600) // 60
-ans_s = left_sec % 60
-
-print(f'{ans_h:02d}:{ans_m:02d}:{ans_s:02d}')
+print(f'{real_hour:02}:{real_minute:02}:{real_second:02}')
