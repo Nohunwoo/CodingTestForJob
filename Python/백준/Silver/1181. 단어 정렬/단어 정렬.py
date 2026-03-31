5b5dfccf392957cad17468b86b5a2c8840c77afb
Nohunwoo <163727448+Nohunwoo@users.noreply.github.com>
2026-03-31 10:32:14 +0900

    [Silver V] Title: 단어 정렬, Time: 560 ms, Memory: 37024 KB -BaekjoonHub

---
+++ Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,9 @@
+n = int(input())
+word_list = []
+for i in range(n):
+    word_list.append(input())
+word_list = list(set(word_list))
+word_list.sort(key=lambda x: (len(x), x))
+
+for i in range(len(word_list)):
+    print(word_list[i])
